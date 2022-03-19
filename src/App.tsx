cf7d6c51099d28970a5f0d71458896fb99d98c87
waohqc <1354214576@qq.com>
2022-03-20 01:08:40 +0800

    尝试使用React写第一个页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Menu, List } from "antd"
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons"
+import "./App.css"
+import InfoItem from "./InfoItem/InfoItem"
+import { Extension } from "typescript"
+const { SubMenu } = Menu
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class Sider extends React.Component {
+  handleClick = (e: any) => {
+    console.log("click ", e)
+  }
+
+  render() {
+    const listData = [{}, {}, {}, {}, {}, {}, {}]
+    return (
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Menu
+          onClick={this.handleClick}
+          style={{ width: 256, height: "100vh" }}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+            <Menu.ItemGroup key="g1" title="Item 1">
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup key="g2" title="Item 2">
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            icon={<AppstoreOutlined />}
+            title="Navigation Two"
+          >
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu
+            key="sub4"
+            icon={<SettingOutlined />}
+            title="Navigation Three"
+          >
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.Item key="11">Option 11</Menu.Item>
+            <Menu.Item key="12">Option 12</Menu.Item>
+          </SubMenu>
+        </Menu>
+        <List
+          style={{ height: "100%" }}
+          itemLayout="horizontal"
+          size="large"
+          dataSource={listData}
+          renderItem={item => <InfoItem />}
+        ></List>
+      </div>
+    )
+  }
 
-export default App;
+  _getInfos() {
+    let infoItems = []
+    for (let index = 0; index < 10; index++) {
+      infoItems.push(<InfoItem />)
+    }
+    return infoItems
+  }
+}
---
+++ src/InfoItem/InfoItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class InfoItem extends React.Component {
+    render() {
+        return(
+            <div style={{width: 300, height: 100,marginBottom: 20, background: 'orange', borderRadius: 10}}></div>
+        )
+    }
+}
