d5505d963432f665aa0edf85146d9eecc51b3a5a
waohqc <1354214576@qq.com>
2022-03-20 02:09:52 +0800

    waoh搜索页

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SearchPage from './App2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
