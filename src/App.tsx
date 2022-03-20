601c0ff797c265e33c7cd09758da34bf4221a939
waohqc <1354214576@qq.com>
2022-03-20 16:09:35 +0800

    antd学习&使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,85 @@
-import React from "react"
-import { Menu, List } from "antd"
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from "@ant-design/icons"
 import "./App.css"
-import InfoItem from "./InfoItem/InfoItem"
-import { Extension } from "typescript"
-const { SubMenu } = Menu
+import React from "react"
+import { Input, Image, Select } from "antd"
+import { AudioOutlined } from "@ant-design/icons"
+import logo from "./waohfun_logo.png"
+const { Search } = Input
+const { Option } = Select
 
-export default class Sider extends React.Component {
-  handleClick = (e: any) => {
-    console.log("click ", e)
+const selectBefore = (
+  <Select defaultValue="Google" className="select-before">
+    <Option value="Baidu">Baidu</Option>
+    <Option value="Google">Google</Option>
+  </Select>
+)
+
+enum searchOption {
+  Baidu = "Baidu",
+  Google = "Google",
+}
+export default class SearchPage extends React.Component {
+  searchPrefix: string = 'Google'
+  _getSearchPrefixUrl(): string {
+    if (this.searchPrefix === searchOption.Google) {
+      return 'https://www.google.com/search?q='
+    } 
+    switch (this.searchPrefix) {
+      case searchOption.Baidu:
+        return 'https://www.baidu.com/s?wd='
+      case searchOption.Google:
+      default:
+        return 'https://www.google.com/search?q='
+    }
   }
-
   render() {
-    const listData = [{}, {}, {}, {}, {}, {}, {}]
     return (
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <Menu
-          onClick={this.handleClick}
-          style={{ width: 256, height: "100vh" }}
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1"]}
-          mode="inline"
-        >
-          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-            <Menu.ItemGroup key="g1" title="Item 1">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup key="g2" title="Item 2">
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            icon={<AppstoreOutlined />}
-            title="Navigation Two"
-          >
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="7">Option 7</Menu.Item>
-              <Menu.Item key="8">Option 8</Menu.Item>
-            </SubMenu>
-          </SubMenu>
-          <SubMenu
-            key="sub4"
-            icon={<SettingOutlined />}
-            title="Navigation Three"
-          >
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
-          </SubMenu>
-        </Menu>
-        <List
-          style={{ height: "100%" }}
-          itemLayout="horizontal"
-          size="large"
-          dataSource={listData}
-          renderItem={item => <InfoItem />}
-        ></List>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "30vh",
+        }}
+      >
+        <div style={{ paddingBottom: 20 }}>
+          <Image
+            preview={false}
+            height={(68 * 2) / 3}
+            width={(344 * 2) / 3}
+            src={logo}
+          />
+        </div>
+        <Search
+          addonBefore={
+            <Select
+              onChange={this._onSelectOptionChange}
+              defaultValue="Google"
+              className="select-before"
+            >
+              <Option value="Baidu">Baidu</Option>
+              <Option value="Google">Google</Option>
+            </Select>
+          }
+          allowClear
+          enterButton
+          style={{ width: "33vw" }}
+          placeholder="搜一搜"
+          onSearch={this._onSearch}
+        />
       </div>
     )
   }
 
-  _getInfos() {
-    let infoItems = []
-    for (let index = 0; index < 10; index++) {
-      infoItems.push(<InfoItem />)
+  _onSearch = (keyWord?: string) => {
+    if (!keyWord?.length) {
+      return
     }
-    return infoItems
+    window.open(`${this._getSearchPrefixUrl()}${keyWord}`)
+  }
+
+  _onSelectOptionChange = (value: string) => {
+    this.searchPrefix = value
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import SearchPage from './App2';
+import SearchPage from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
