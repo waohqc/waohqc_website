70eba07e5139fb6b09ce62dcfcca48a868d2ac14
waohqc <1354214576@qq.com>
2022-03-26 18:59:31 +0800

    增加IPC信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css"
 import React from "react"
-import { Input, Image, Select } from "antd"
+import { Input, Image, Select, Button } from "antd"
 import { AudioOutlined } from "@ant-design/icons"
 import logo from "./waohfun_logo.png"
 const { Search } = Input
@@ -18,19 +18,21 @@ enum searchOption {
   Google = "Google",
 }
 export default class SearchPage extends React.Component {
-  searchPrefix: string = 'Google'
+  searchPrefix: string = "Google"
   _getSearchPrefixUrl(): string {
     if (this.searchPrefix === searchOption.Google) {
-      return 'https://www.google.com/search?q='
-    } 
+      return "https://www.google.com/search?q="
+    }
     switch (this.searchPrefix) {
       case searchOption.Baidu:
-        return 'https://www.baidu.com/s?wd='
+        return "https://www.baidu.com/s?wd="
       case searchOption.Google:
       default:
-        return 'https://www.google.com/search?q='
+        return "https://www.google.com/search?q="
     }
   }
+  icpUrl: string = "https://beian.miit.gov.cn"
+
   render() {
     return (
       <div
@@ -44,14 +46,11 @@ export default class SearchPage extends React.Component {
         }}
       >
         <div style={{ paddingBottom: 20 }}>
-          <Image
-            preview={false}
-            height={594 / 6}
-            width={1310 / 6}
-            src={logo}
-          />
+          <Image preview={false} height={594 / 6} width={1310 / 6} src={logo} />
         </div>
-        <Search
+        <text>个人知识总结学习</text>
+        <text>内容正等待补充</text>
+        {/* <Search
           addonBefore={
             <Select
               onChange={this._onSelectOptionChange}
@@ -67,7 +66,16 @@ export default class SearchPage extends React.Component {
           style={{ width: "33vw" }}
           placeholder="搜一搜"
           onSearch={this._onSearch}
-        />
+        /> */}
+        <div className="officalRecord">
+          <Button
+            type="link"
+            style={{ color: "lightgray" }}
+            onClick={this._onClickIcp}
+          >
+            皖ICP备2022003636号
+          </Button>
+        </div>
       </div>
     )
   }
@@ -81,5 +89,9 @@ export default class SearchPage extends React.Component {
 
   _onSelectOptionChange = (value: string) => {
     this.searchPrefix = value
+  }
+
+  _onClickIcp = () => {
+    window.open(`${this.icpUrl}`)
   }
 }
